6850c36499ac0fbb9c5ab9048f53416ea8a77a6c
Huzaifa7524 <huzaifatahir7524@gmail.com>
2023-12-18 16:04:40 +0500

    updated

--- american_ai_tutor/american_ai_tutor/settings.py
+++ american_ai_tutor/american_ai_tutor/settings.py
@@ -90,11 +90,22 @@ WSGI_APPLICATION = 'american_ai_tutor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myaitutor',
+        'USER': 'aitutorapp',
+        'PASSWORD': 'aitutor123',
+        'HOST': 'aitutor.c3wuqkeoudy7.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }   
 }
 
 
